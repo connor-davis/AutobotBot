c6abd184f591461227b32319af8793483d2442a7
BuildTools <unconfigured@null.spigotmc.org>
2023-01-16 00:49:45 +0200

    Add payment command

---
+++ src/commands/public/payment.js
@@ -0,0 +1,50 @@
+const {
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  EmbedBuilder,
+} = require("discord.js");
+
+module.exports = {
+  name: "payment",
+  data: new SlashCommandBuilder()
+    .setName("payment")
+    .setDescription("Receive my payment details."),
+  /**
+   * @param {ChatInputCommandInteraction} interaction
+   */
+  execute: (interaction, client, logger) => {
+    const paymentInfoEmbed = new EmbedBuilder()
+      .setTitle("Autobot Payment Details")
+      .setDescription(
+        "Below you can find payment details for purchasing Autobot."
+      )
+      .setColor(0x34d399)
+      .addFields([
+        {
+          name: "Paypal",
+          value: "https://paypal.me/lonewolftech",
+        },
+        {
+          name: "Bank Account Number",
+          value: "1458805830",
+          inline: true,
+        },
+        {
+          name: "Bank Branch Code",
+          value: "470010",
+          inline: true,
+        },
+        {
+          name: "Bank Name",
+          value: "Capitec",
+          inline: true,
+        },
+      ])
+      .setAuthor({
+        name: "Requested by: " + interaction.user.username,
+        iconURL: interaction.user.avatarURL(),
+      });
+
+    interaction.reply({ embeds: [paymentInfoEmbed], ephemeral: true });
+  },
+};
