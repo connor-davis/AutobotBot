0ec3c296a07f3b73c6f751d9f7348654eb8cde6c
BuildTools <unconfigured@null.spigotmc.org>
2023-01-16 01:11:22 +0200

    Update ping command

--- src/commands/public/ping.js
+++ src/commands/public/ping.js
@@ -18,7 +18,13 @@ module.exports = {
     const pingEmbed = new EmbedBuilder()
       .setTitle("Autobot Ping Information")
       .setColor(0x34d399)
-      .addFields([{ name: "WS Latency", value: client.ws.ping }])
+      .addFields([
+        { name: "WS Latency", value: client.ws.ping + "ms" },
+        {
+          name: "Latency",
+          value: m.createdTimestamp - message.createdTimestamp + "ms",
+        },
+      ])
       .setFooter({
         text: "Requested by " + interaction.user.username,
         iconURL: interaction.user.avatarURL(),
